7574f404a321a93c30da40597c6a4369ce1cd28e
Stephan <stephan-zager@web.de>
2025-03-11 17:55:03 +0100

    first commit

---
+++ src/app/main-page/lading-page/lading-page.component.ts
@@ -0,0 +1,12 @@
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-lading-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './lading-page.component.html',
+  styleUrl: './lading-page.component.scss'
+})
+export class LadingPageComponent { }
+
+
---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,42 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LadingPageComponent } from "./lading-page/lading-page.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { MySkillsComponent } from "./my-skills/my-skills.component";
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [LadingPageComponent, AboutMeComponent, MySkillsComponent],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.scss'
+})
+export class MainPageComponent implements AfterViewInit {
+
+  @ViewChild('mainPageContainer', { static: false }) mainPageContainer!: ElementRef;
+  private lastScrollTop = 0;
+
+  ngAfterViewInit() {
+    if (this.mainPageContainer) {
+      this.mainPageContainer.nativeElement.addEventListener('scroll', this.onScroll.bind(this));
+    }
+  }
+
+  onScroll() {
+    const testElement = document.querySelector('.test') as HTMLElement;
+    console.log('Scroll event detected');
+
+    if (testElement) {
+      const scrollPosition = this.mainPageContainer.nativeElement.scrollTop;
+
+      if (scrollPosition > this.lastScrollTop) {
+        testElement.classList.add('move-bottom-left');
+        testElement.classList.remove('move-top-center');
+      } else {
+        testElement.classList.add('move-top-center');
+        testElement.classList.remove('move-bottom-left');
+      }
+
+      this.lastScrollTop = scrollPosition <= 0 ? 0 : scrollPosition;
+    }
+  }
+}
