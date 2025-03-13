d2933f3e30c215cc715fe591851a298beaf939b5
Stephan <stephan-zager@web.de>
2025-03-13 20:21:50 +0100

    scroll down animation and compnente

--- src/app/main-page/lading-page/lading-page.component.ts
+++ src/app/main-page/lading-page/lading-page.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { IconBarComponent } from "../../shared/icon-bar/icon-bar.component";
+import { ScrollDownComponent } from "./scroll-down/scroll-down.component";
 
 @Component({
   selector: 'app-lading-page',
   standalone: true,
-  imports: [IconBarComponent],
+  imports: [IconBarComponent, ScrollDownComponent],
   templateUrl: './lading-page.component.html',
   styleUrl: './lading-page.component.scss'
 })
