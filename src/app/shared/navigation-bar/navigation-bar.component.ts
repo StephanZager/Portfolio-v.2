36ecf7f5446bcd832711ca145744bb1b2f17c553
Stephan <stephan-zager@web.de>
2025-03-26 20:30:02 +0100

    navigations bar # a link

--- src/app/shared/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
 
+  scrollLink = [
+    { id: 'ladingPage', label: 'Landing Page' },
+    { id: 'aboutMe', label: 'About Me' },
+    { id: 'mySkills', label: 'My Skills' },
+    { id: 'myWork', label: 'My Work' },
+    { id: 'referenzen', label: 'Referenzen' },
+    { id: 'contact', label: 'Contact' }
+  ];
+
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView();
+    }
+  }
 }
