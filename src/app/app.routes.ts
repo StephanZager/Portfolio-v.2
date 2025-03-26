c7a6a2ddc410f56def518ee418ae203fdd85cce8
Stephan <stephan-zager@web.de>
2025-03-26 21:52:56 +0100

    navigation with router links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './main-page/contact/contact.component';
+import { ReferenzenComponent } from './main-page/referenzen/referenzen.component';
+import { MyWorkComponent } from './main-page/my-work/my-work.component';
+import { MySkillsComponent } from './main-page/my-skills/my-skills.component';
+import { AboutMeComponent } from './main-page/about-me/about-me.component';
+import { LadingPageComponent } from './main-page/lading-page/lading-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'ladingPage', component: LadingPageComponent },
+    { path: 'aboutMe', component: AboutMeComponent },
+    { path: 'mySkills', component: MySkillsComponent },
+    { path: 'myWork', component: MyWorkComponent },
+    { path: 'referenzen', component: ReferenzenComponent },
+    { path: 'contact', component: ContactComponent },
+];
--- src/app/shared/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,28 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
 
   scrollLink = [
-    { id: 'ladingPage', label: 'Landing Page' },
-    { id: 'aboutMe', label: 'About Me' },
-    { id: 'mySkills', label: 'My Skills' },
-    { id: 'myWork', label: 'My Work' },
-    { id: 'referenzen', label: 'Referenzen' },
-    { id: 'contact', label: 'Contact' }
+    { id: 'ladingPage' },
+    { id: 'aboutMe' },
+    { id: 'mySkills' },
+    { id: 'myWork' },
+    { id: 'referenzen' },
+    { id: 'contact' }
   ];
+  constructor(private router: Router) { }
+
+  activeRoute: string = '';
+
+  ngOnInit() {
+    
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        
+        this.activeRoute = event.urlAfterRedirects.split('/')[1];
+      }
+    });
+  }
 
   scrollToSection(sectionId: string) {
-    const element = document.getElementById(sectionId);
-    if (element) {
-      element.scrollIntoView();
-    }
+    // Verhindere das Neuladen der Seite
+    this.router.navigate([], { fragment: sectionId }).then(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
   }
 }
