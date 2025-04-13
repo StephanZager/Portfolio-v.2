06eea91b1e23eaba2e6f8caa3fb099a40fa8477f
Stephan <stephan-zager@web.de>
2025-04-13 10:38:41 +0200

    about me menu created

--- src/app/main-page/about-me/about-me.component.ts
+++ src/app/main-page/about-me/about-me.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { PopUpMenuComponent } from "./pop-up-menu/pop-up-menu.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [PopUpMenuComponent, CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
 
+  isMenuVisible = false;
+
+  openMenu() {
+    this.isMenuVisible = true;
+  }
+
+  closeMenu() {
+    this.isMenuVisible = false;
+  }
 }
---
+++ src/app/main-page/about-me/pop-up-menu/pop-up-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './pop-up-menu.component.html',
+  styleUrl: './pop-up-menu.component.scss'
+})
+export class PopUpMenuComponent {
+
+}
