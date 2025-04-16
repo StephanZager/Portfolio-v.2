3eea0be527f88e93d8a62794d8522f53799bb73d
Stephan <stephan-zager@web.de>
2025-04-16 21:18:54 +0200

    loading page translation

--- src/app/main-page/about-me/about-me.component.ts
+++ src/app/main-page/about-me/about-me.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PopUpMenuComponent } from "./pop-up-menu/pop-up-menu.component";
 import { CommonModule } from '@angular/common';
+import { TranslateModule} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [PopUpMenuComponent, CommonModule],
+  imports: [PopUpMenuComponent, CommonModule,TranslateModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
--- src/app/main-page/lading-page/lading-page.component.ts
+++ src/app/main-page/lading-page/lading-page.component.ts
@@ -1,14 +1,18 @@
 import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { IconBarComponent } from "../../shared/icon-bar/icon-bar.component";
 import { ScrollDownComponent } from "./scroll-down/scroll-down.component";
+import { TranslateModule} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-lading-page',
   standalone: true,
-  imports: [IconBarComponent, ScrollDownComponent],
+  imports: [IconBarComponent, ScrollDownComponent, TranslateModule],
   templateUrl: './lading-page.component.html',
   styleUrl: './lading-page.component.scss'
 })
-export class LadingPageComponent { }
+export class LadingPageComponent {
+
+
+}
 
 
