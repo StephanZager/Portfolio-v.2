046807b40a72c9b92f277d22529c4b1c68174fef
Stephan <stephan-zager@web.de>
2025-04-17 22:10:12 +0200

    my skills translated

--- src/app/main-page/my-skills/my-skills.component.ts
+++ src/app/main-page/my-skills/my-skills.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ContinualLearningContentComponent } from "./continual-learning-content/continual-learning-content.component";
 import { SkillsComponent } from "./skills/skills.component";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [ContinualLearningContentComponent, SkillsComponent],
+  imports: [ContinualLearningContentComponent, SkillsComponent,TranslateModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
