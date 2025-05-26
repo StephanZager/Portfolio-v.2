db5a65a25ecfb00081781991c7d7f5c405e97622
Stephan <stephan-zager@web.de>
2025-05-26 16:57:31 +0200

    contact arrow

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { CircleComponent } from "./circle/circle.component";
 import { ContactFormComponent } from "./contact-form/contact-form.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
+import { ArrowUpComponent } from "../../shared/arrow-up/arrow-up.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CircleComponent, ContactFormComponent, FooterComponent],
+  imports: [CircleComponent, ContactFormComponent, FooterComponent, ArrowUpComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  scrollToLanding() {
+    console.log('d')
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
