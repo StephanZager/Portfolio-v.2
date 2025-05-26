0d809d67b975d30242b8328aa923cecac998177a
Stephan <stephan-zager@web.de>
2025-05-26 16:23:04 +0200

    footer crated

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CircleComponent } from "./circle/circle.component";
 import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CircleComponent, ContactFormComponent],
+  imports: [CircleComponent, ContactFormComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
