2e69c65fe9d82b66ffadf71b129d5c461a1be033
Stephan <stephan-zager@web.de>
2025-05-26 15:52:54 +0200

    cotnact responsiv to 800 px

--- src/app/main-page/contact/contact-form/contact-form.component.ts
+++ src/app/main-page/contact/contact-form/contact-form.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
@@ -33,6 +34,16 @@ export class ContactFormComponent {
   emailFocusLabel: boolean = false;
   messageFocusLabel: boolean = false;
 
+  agbChecked: boolean = false;
+
+  emailInvalid: boolean = false;
+  emailInvalidText: string = 'Bitte gib eine gültige E-Mail-Adresse ein.';
+
+  onCheckboxChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.agbChecked = input.checked;
+  }
+
   onBlurName(event: FocusEvent) {
     const input = event.target as HTMLInputElement;
     if (!input.value) {
@@ -46,18 +57,26 @@ export class ContactFormComponent {
     }
   }
 
-  onBlurEmail(event: FocusEvent) {
-    const input = event.target as HTMLInputElement;
-    if (!input.value) {
-      this.emailPlaceholder = 'Bitte geben Sie Ihre Email ein';
-      input.classList.add('placeholder-error');
-      this.emailError = true;
-      this.emailFocusLabel = false;
-    } else {
-      input.classList.remove('placeholder-error');
-      this.emailError = false;
-    }
+onBlurEmail(event: FocusEvent) {
+  const input = event.target as HTMLInputElement;
+  if (!input.value) {
+    this.emailPlaceholder = 'Bitte geben Sie Ihre Email ein';
+    input.classList.add('placeholder-error');
+    this.emailError = true;
+    this.emailFocusLabel = false;
+    this.emailInvalid = false;
+  } else if (!input.checkValidity()) {
+    this.emailPlaceholder = this.emailInvalidText; 
+    input.classList.add('placeholder-error');
+    this.emailError = false;
+    this.emailInvalid = true;
+    this.email = ''; 
+  } else {
+    input.classList.remove('placeholder-error');
+    this.emailError = false;
+    this.emailInvalid = false;
   }
+}
 
   onBlurMessage(event: FocusEvent) {
     const input = event.target as HTMLTextAreaElement;
@@ -75,6 +94,8 @@ export class ContactFormComponent {
   onSubmit(event: Event) {
     event.preventDefault();
     this.isSubmitting = true;
+    console.log(this.name,this.email,this.message)
+    this.resetForm();
   }
 
   focusName(event: FocusEvent) {
@@ -92,7 +113,19 @@ export class ContactFormComponent {
     this.messageFocusLabel = true;
   }
 
-  test() {
-    console.log('hallod')
-  }
+  resetForm() {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+    this.agbChecked = false;
+    this.nameError = false;
+    this.emailError = false;
+    this.messageError = false;
+    this.nameFocus = false;
+    this.emailFocus = false;
+    this.messageFocus = false;
+    this.nameFocusLabel = false;
+    this.emailFocusLabel = false;
+    this.messageFocusLabel = false;
+}
 }
