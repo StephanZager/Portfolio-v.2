36a5dbbb97de16cf73f8f864eb2e067b6530c5d0
Stephan <stephan-zager@web.de>
2025-06-07 14:55:22 +0200

    referenzen translate

--- src/app/main-page/referenzen/referenzen-item/referenzen-item.component.ts
+++ src/app/main-page/referenzen/referenzen-item/referenzen-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-referenzen-item',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './referenzen-item.component.html',
   styleUrl: './referenzen-item.component.scss'
 })
--- src/app/main-page/referenzen/referenzen.component.ts
+++ src/app/main-page/referenzen/referenzen.component.ts
@@ -2,31 +2,32 @@ import { Component } from '@angular/core';
 import { ReferenzenItemComponent } from "./referenzen-item/referenzen-item.component";
 import { link } from 'fs';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-referenzen',
   standalone: true,
-  imports: [ReferenzenItemComponent, CommonModule],
+  imports: [ReferenzenItemComponent, CommonModule, TranslateModule],
   templateUrl: './referenzen.component.html',
   styleUrl: './referenzen.component.scss'
 })
 export class ReferenzenComponent {
   referenzenData = [
     {
-      name: 'Johanne Lennike',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut cum voluptatem deleniti qui necessitatibus iure nihil, illo illum, cupiditate tempore nesciunt dolore ipsa. Laborum eius officiis accusamus distinctio reprehenderit dicta.',
+      name: 'Johannes Linnecke',
+      description: 'referenzen.feedback.johannes',
       qualifikation: 'Team Partner',
       link: '#'
     },
     {
-      name: 'Lisa Hübler',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut cum voluptatem deleniti qui necessitatibus iure nihil, illo illum, cupiditate tempore nesciunt dolore ipsa. Laborum eius officiis accusamus distinctio reprehenderit dicta.',
+      name: 'Andrei Butalov',
+      description: 'referenzen.feedback.andrei',
       qualifikation: 'Team Partner',
       link: '#'
     },
     {
-      name: 'Lukas Zager',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut cum voluptatem deleniti qui necessitatibus iure nihil, illo illum, cupiditate tempore nesciunt dolore ipsa. Laborum eius officiis accusamus distinctio reprehenderit dicta.',
+      name: 'Jens Natzick',
+      description: 'referenzen.feedback.jens',
       qualifikation: 'Team Partner',
       link: '#'
     },
